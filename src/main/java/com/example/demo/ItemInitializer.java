4f2abdc6b9c55f66b5260f2d497bb785185989ac
Asik_Ali_ <asketltester@gmail.com>
2020-10-25 02:48:25 -0500

    update

--- src/main/java/com/example/demo/ItemInitializer.java
+++ src/main/java/com/example/demo/ItemInitializer.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Component;
@@ -30,7 +31,7 @@ public class ItemInitializer implements CommandLineRunner  {
         this.ItemRepository = itemRepository;
     }
 
-    @Override
+    @Override  
     public void run(String... args) throws Exception {
         Stream.of("Lining", "PUMA", "Bad Boy", "Air Jordan", "Nike", "Adidas", "Reebok")
                 .forEach(item -> ItemRepository.save(new Item(item)));
